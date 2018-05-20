ee94d33b40bccdd5d147e554e68ddcfcb35340c3
Richard Powell <richardp@apple.com>
2018-05-20 12:25:04 -0700

    some more wip

--- kilo.cpp
+++ kilo.cpp
@@ -207,14 +207,15 @@ struct editorSyntax HLDB[] = {
 
 class TermConfig {
 public:
-    TermConfig(int fd);
+    TermConfig(int ifd, int ofd);
     ~TermConfig();
 
-    std::pair<int, int> getWindowSize(int ifd, int ofd);
+    std::pair<int, int> getWindowSize();
 
 private:
     termios configTerm(termios orig_termios);
-    int m_fd{};
+    int m_ifd{};
+    int m_ofd{};
     termios orig_termios; /* In order to restore at exit.*/
 };
 
@@ -222,12 +223,12 @@ static std::unique_ptr<TermConfig> T_;
 
 
 /* Raw mode: 1960 magic shit. */
-TermConfig::TermConfig(int fd) : m_fd(fd) {
-    if (!isatty(m_fd)) {
+TermConfig::TermConfig(int ifd, int ofd) : m_ifd(ifd), m_ofd(ofd) {
+    if (!isatty(m_ifd)) {
         errno = ENOTTY;
         throw std::runtime_error("not a tty");
     }
-    if (tcgetattr(m_fd,&orig_termios) == -1) {
+    if (tcgetattr(m_ifd,&orig_termios) == -1) {
         errno = ENOTTY;
         throw std::runtime_error("cannot get terminal attributes");
     }
@@ -235,16 +236,16 @@ TermConfig::TermConfig(int fd) : m_fd(fd) {
     auto raw = configTerm(orig_termios);
 
     /* put terminal in raw mode after flushing */
-    if (tcsetattr(m_fd,TCSAFLUSH,&raw) < 0) {
+    if (tcsetattr(m_ifd,TCSAFLUSH,&raw) < 0) {
         errno = ENOTTY;
         throw std::runtime_error("cannot set terminal attributes");
     }
 }
 
 TermConfig::~TermConfig() {
-    write(m_fd, "\x1b[2J", 4);
-    write(m_fd, "\x1b[H", 3);
-    tcsetattr(m_fd,TCSAFLUSH,&orig_termios);
+    write(m_ifd, "\x1b[2J", 4);
+    write(m_ifd, "\x1b[H", 3);
+    tcsetattr(m_ifd,TCSAFLUSH,&orig_termios);
 }
 
 termios TermConfig::configTerm(termios orig_termios) {
@@ -346,7 +347,7 @@ void setCursorPosition(int /*ifd*/, int ofd, int row, int col) {
 // Try to get the number of columns in the current terminal. If the ioctl()
 // call fails the function will try to query the terminal itself.
 // Returns 0 on success, -1 on error.
-std::pair<int, int> TermConfig::getWindowSize(int ifd, int ofd) {
+std::pair<int, int> TermConfig::getWindowSize() {
     struct winsize ws;
     if (ioctl(1, TIOCGWINSZ, &ws) != -1 && ws.ws_col != 0) {
         return { ws.ws_row, ws.ws_col };
@@ -354,13 +355,13 @@ std::pair<int, int> TermConfig::getWindowSize(int ifd, int ofd) {
     // if that failed, try reading the terminal itself
 
     // save the cursor position
-    auto orig = getCursorPosition(ifd,ofd);
+    auto orig = getCursorPosition(m_ifd,m_ofd);
 
-    setCursorPosition(ifd, ofd, 999, 999);
-    auto size = getCursorPosition(ifd,ofd);
+    setCursorPosition(m_ifd, m_ofd, 999, 999);
+    auto size = getCursorPosition(m_ifd,m_ofd);
 
     // restore position
-    setCursorPosition(ifd, ofd, orig.first, orig.second);
+    setCursorPosition(m_ifd, m_ofd, orig.first, orig.second);
 
     return size;
 }
@@ -1166,7 +1167,7 @@ int editorFileWasModified(editorConfig& E) {
 
 editorConfig::editorConfig()
 {
-    std::tie(m_screenrows, m_screencols) = T_->getWindowSize(STDIN_FILENO,STDOUT_FILENO);
+    std::tie(m_screenrows, m_screencols) = T_->getWindowSize();
     m_screenrows -= 2; /* Get room for status bar. */
 }
 
@@ -1176,7 +1177,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    T_ = std::make_unique<TermConfig>(STDIN_FILENO);
+    T_ = std::make_unique<TermConfig>(STDIN_FILENO, STDOUT_FILENO);
     E_.editorSelectSyntaxHighlight(argv[1]);
     E_.editorOpen(argv[1]);
     E_.editorSetStatusMessage(
